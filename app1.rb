3beb9c8cd9e9d2393c4adbb61197541639139433
fsiammenou <fsiammenou@city.academic.gr>
2013-11-21 00:04:28 +0200

    fotini's final

--- app1.rb
+++ app1.rb
@@ -1,5 +1,5 @@
 
-class App < Sinatra::Application
+class App < Sinatra::Application 
   #we may add a setting for the error to be showm or not
   #and may use different configuration for users and programmers (us)
   #on the other hand
@@ -12,23 +12,19 @@ class App < Sinatra::Application
   register do
     def auth(parm)
       condition do
-        redirect "/loginRequired" unless is_userloggedin?      #Þ "/login_required" åöïóïí ðñïóèåóù åíá ôåôïéï route
+        redirect "/loginRequired" unless is_userloggedin?     
       end
     end
   end
 
-       
-  #enallaktika admin part
-#  if !is_userloggedin? then 
-#     redirect to "/login_required"
-#  end if  
+ 
    
   get "/loginRequired" do
     haml:loginRequired
   end
   
 
-  get "/logout" do           #ðñïóèçêç logout route
+  get "/logout" do          
     session[:user_id] = nil
     redirect to "/"
   end
@@ -50,8 +46,8 @@ class App < Sinatra::Application
   end 
 
 
-  before do       #ðñéí áðü ïôéäÞðïôå
-    if session[:user_id]==nil then 
+  before do      
+    if session[:user_id]==nil  
        @loggedinuser = nil
     else
        @loggedinuser = User[session[:user_id]]
@@ -74,11 +70,6 @@ class App < Sinatra::Application
     else
        "There are saved posts!"
        haml:publicAllPosts, :locals=>{:posts=>posts}
-        #output=''
-	#posts.each do |p|
-	#   output << "#{p.title} <br />"
-	#end
-        #return output
     end    
     #view of posts list, search abilities: 
 	# date selection
@@ -92,8 +83,7 @@ class App < Sinatra::Application
     #in :params somehow we ll have the id of a post to show 
     #haml:viewPost
     #containing the form for comment submition
-      #{:action => "/viewPost", :method => "post"}
-   #AND a browse link -->redirects to /browsePosts
+    #AND a browse link -->redirects to /browsePosts
     "You now should view post with id #{params[:postid]}"
     if Post[params[:postid]] == nil
       "Post with id #{params[:postid]} not found"
@@ -112,7 +102,6 @@ class App < Sinatra::Application
       Comment.create(:post_id=>params[:postid], :author=> params[:author],:comment=> params[:comment],:date_added=> DateTime.now)	
       redirect to "/viewPost?postid=#{params[:postid]}"
      end
-    # comment addition information and ok button --> get '/viewPost'
   end
 
 
@@ -136,23 +125,14 @@ class App < Sinatra::Application
         "User logged in"  
         redirect to "/admin"
     end
-#does not work
-#     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
- #    @auth.credentials[0]=params[:username]
-#     @auth.credentials[1]=params[:password]
-#     redirect to "/admin"
+
    end
     # authentication using the params by loginform
     # dinw ta dothenta sto antikeino poy kanei to authentication
     #--> if ok admin part accessible & redirect to /admin
-    # for start I keep them accessible to all
+
   end
 
-  #***********************************************	
-  # we will add later a before "/admin/*" block
-  # so as to check the authentication of the user each time 
-  # one of the adnim part's page  is accessed
-  #************************************************
   get "/admin", :auth => :user do
     haml:adminHome
     #contains links which redirect to newPost and browsePosts (for start)
@@ -171,8 +151,6 @@ class App < Sinatra::Application
   end
 	
   post "/admin/newPost", :auth => :user do    
-	#edw tha eixa ton loggedin user kai tha ekana add post
-	#dinontas mono title kai content
 	newpost = Post.create( :user_id => @loggedinuser.id, :title => params[:title] , :content => params[:content], :date_created => DateTime.now)	
 	"Post created"
 	redirect to("/admin/viewPost?postid=#{newpost.id}")
@@ -188,16 +166,9 @@ class App < Sinatra::Application
     else
        "There are saved posts!"
        haml:allPosts, :locals=>{:posts=>posts}
-        #output=''
-	#posts.each do |p|
-	#   output << "#{p.title} <br />"
-	#end
-        #return output
+     
     end
   
-    #haml:allPosts
-    #view of posts list, search abilities...??? 
-    #multiple forms:
     #link for new post --> get '/admin/newPost'
     #click on a post's title--> redirects to admin/viewPost
 		# with id=the selected
@@ -218,10 +189,6 @@ class App < Sinatra::Application
     #in :params somehow we ll have the id of a post to show 
     #haml:editPost
     #containing save button for changes -->stay in page 
-#-->form submitting :action => "/admin/savePost", method => :post
-	#delete button -->deletes current and redirects to allPosts 
-#--> form submitting "/admin/deletePost", method => :post
-	#browse link -->redirects to /admin/browsePosts
 
     #later add ability for taging
   end
@@ -249,15 +216,7 @@ class App < Sinatra::Application
     end
   end
 
-  post "/admin/deletePost", :auth =>:user do
-    if Post[params[:postid]].exists?
-    	Post[params[:postid]].delete
-    	haml:editPost
-    else
-	"Post with id #{params[:postid]} not found"
-    end
-  end   
-
+  
 
 end
 
